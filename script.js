6be30d2d4432fbee92c68443978c08a0da7d4afe
Igor Bernardes Seixas <igor.bernardes2@gmail.com>
2025-08-07 20:07:31 -0300

    Adicionado novos estilos e textos para as sessões equipe, denúncia e notícias

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-
-document.querySelectorAll('nav a[href^="#"]').forEach(link => {
-  link.addEventListener('click', e => {
+document.querySelectorAll('nav a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
-    const section = document.querySelector(link.getAttribute('href'));
+    const section = document.querySelector(link.getAttribute("href"));
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+      section.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
+
+// Formutário de denúncia
+document.querySelector("form").addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  alert("Denúncia registrada. Obrigado.");
+  event.target.reset();
+}
